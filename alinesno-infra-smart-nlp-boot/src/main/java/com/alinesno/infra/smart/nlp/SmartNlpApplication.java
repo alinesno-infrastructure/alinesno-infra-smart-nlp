f9c285fc2727fcc773b6fb450c62a1ad8017631b
CarrotSea233 <2733614757@qq.com>
2023-11-09 20:51:08 +0800

    feat: 集成mybatis-actable

--- alinesno-infra-smart-nlp-boot/src/main/java/com/alinesno/infra/smart/nlp/SmartNlpApplication.java
+++ alinesno-infra-smart-nlp-boot/src/main/java/com/alinesno/infra/smart/nlp/SmartNlpApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.nlp;
 
+import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableActable
 @EnableInfraSsoApi
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SmartNlpApplication {
 
 	public static void main(String[] args) {
