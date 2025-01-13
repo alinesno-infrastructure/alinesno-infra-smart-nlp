c8dc30e5c0895cac09ecfd51b937cbdb483f6a20
周健(James) <jamesAip@gmail.com>
2025-01-13 08:07:50 +0800

    feat:在LanguageController中导入必要的服务接口和实体类

--- alinesno-infra-smart-nlp-gateway/src/main/java/com/alinesno/infra/smart/nlp/api/controller/LanguageController.java
+++ alinesno-infra-smart-nlp-gateway/src/main/java/com/alinesno/infra/smart/nlp/api/controller/LanguageController.java
@@ -1,11 +1,11 @@
 package com.alinesno.infra.smart.nlp.api.controller;
 
-import com.alinesno.infra.smart.nlp.entity.LanguageEntity;
-import com.alinesno.infra.smart.nlp.service.ILanguageService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.nlp.entity.LanguageEntity;
+import com.alinesno.infra.smart.nlp.service.ILanguageService;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang.builder.ToStringBuilder;
