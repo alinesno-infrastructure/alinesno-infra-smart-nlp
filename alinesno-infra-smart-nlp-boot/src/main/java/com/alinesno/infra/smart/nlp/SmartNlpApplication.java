ff647acc15bc5a89e493f875d533e0c844f7ebf5
周健(James) <jamesAip@gmail.com>
2025-01-13 08:07:50 +0800

    refactor: 简化 SmartNlpApplication 类，移除不必要的注解和方法

--- alinesno-infra-smart-nlp-boot/src/main/java/com/alinesno/infra/smart/nlp/SmartNlpApplication.java
+++ alinesno-infra-smart-nlp-boot/src/main/java/com/alinesno/infra/smart/nlp/SmartNlpApplication.java
@@ -1,32 +1,20 @@
 package com.alinesno.infra.smart.nlp;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ * @author luoxiaodong
+ * @version 1.0.0
  */
-@EnableActable
-@EnableInfraSsoApi
 @SpringBootApplication
-@MapperScan("com.alinesno.infra.smart.nlp.mapper")
 public class SmartNlpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartNlpApplication.class, args);
-	}
-
-	@Bean
-	public LogAspect getLogAspect(){
-		return new LogAspect() ;
 	}
 
 }
