980c7f9cdd7c4da8fab2b4876f88bb94207d8320
周健(James) <jamesAip@gmail.com>
2025-01-13 08:07:50 +0800

    refactor: 优化 ApplicationEntity 类的导入顺序并添加注释

--- alinesno-infra-smart-nlp-facade/src/main/java/com/alinesno/infra/smart/nlp/entity/ApplicationEntity.java
+++ alinesno-infra-smart-nlp-facade/src/main/java/com/alinesno/infra/smart/nlp/entity/ApplicationEntity.java
@@ -1,11 +1,11 @@
 package com.alinesno.infra.smart.nlp.entity;
 
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
-import lombok.Data;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
