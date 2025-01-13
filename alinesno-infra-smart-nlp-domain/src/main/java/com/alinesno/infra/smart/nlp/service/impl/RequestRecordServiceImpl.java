13d6c2764f3f31d00279c04021101492a31fb538
周健(James) <jamesAip@gmail.com>
2025-01-13 08:07:50 +0800

    refactor(RequestRecordServiceImpl): 调整导入顺序并优化代码结构

--- alinesno-infra-smart-nlp-domain/src/main/java/com/alinesno/infra/smart/nlp/service/impl/RequestRecordServiceImpl.java
+++ alinesno-infra-smart-nlp-domain/src/main/java/com/alinesno/infra/smart/nlp/service/impl/RequestRecordServiceImpl.java
@@ -1,13 +1,12 @@
 package com.alinesno.infra.smart.nlp.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.nlp.entity.RequestRecordEntity;
 import com.alinesno.infra.smart.nlp.mapper.RequestRecordMapper;
 import com.alinesno.infra.smart.nlp.service.IRequestRecordService;
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
