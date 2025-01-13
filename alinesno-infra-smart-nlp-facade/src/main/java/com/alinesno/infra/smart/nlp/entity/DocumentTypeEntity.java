5051529dddd9d4623626f024f904f2543ad133d9
周健(James) <jamesAip@gmail.com>
2025-01-13 08:07:49 +0800

    feat:在 DocumentTypeEntity 类中添加 Lombok 注解

--- alinesno-infra-smart-nlp-facade/src/main/java/com/alinesno/infra/smart/nlp/entity/DocumentTypeEntity.java
+++ alinesno-infra-smart-nlp-facade/src/main/java/com/alinesno/infra/smart/nlp/entity/DocumentTypeEntity.java
@@ -3,10 +3,10 @@ package com.alinesno.infra.smart.nlp.entity;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 文档类型
